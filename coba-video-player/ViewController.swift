f7bd583c35eb597a301a8edbdc5e4f3715fb877a
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-08-18 08:09:55 +0700

    Initial Commit

---
+++ coba-video-player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coba-video-player
+//
+//  Created by Muhammad Rifki Widadi on 18/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
